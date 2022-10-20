7ce7caf4edfef918c744ce7ac7ab9d3226b380aa
anari931531 <ansari931531gmail.com>
2022-10-20 18:46:42 +0530

    frist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Main  from './component/Main';
+import  style from'./component/style.css';
+
+function App() {
+  return (
+    <>
+    <Main/>
+    </>
+  );
+}
+
+export default App;
